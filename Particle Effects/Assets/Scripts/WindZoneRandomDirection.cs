6a5971966e78b1cbe544b6d762393777e61cfb4f
bihailvlin <1119791511@qq.com>
2020-10-07 13:27:50 +0800

    添加Unity3D使用粒子系统实现树叶飘落的效果

---
+++ Particle Effects/Assets/Scripts/WindZoneRandomDirection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindZoneRandomDirection : MonoBehaviour {
+
+    float timer = 0;
+    float randdomTime = 5;
+	void Update ()
+    {
+        timer += Time.deltaTime;
+        if (timer> randdomTime)
+        {
+            randdomTime = Random.Range(2, 5);
+
+            Vector3 vectorRotate = new Vector3(Random.Range(0, 180), Random.Range(0, 360), 0);
+            this.transform.rotation =   Quaternion.Euler(vectorRotate);
+            timer = 0;
+
+        }
+	}
+}
